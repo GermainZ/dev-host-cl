2ecddbeaad1c4c155e4f94e51fac3bf190576aac
GermainZ <germanosz@gmail.com>
2013-06-12 21:48:50 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,34 @@
 dev-host-cl
 ===========
 
-Upload files to http://d-h.st (Dev-Host) from the command line
+Upload files to http://d-h.st (Dev-Host) from the command line.
+
+Usage
+=====
+
+* Usage: devhost.py [-h] [-u USERNAME] [-p PASSWORD] [-d FILE_DESC]
+                    [-c FILE_CODE] [-pb {0,1}] [-f UPLOAD_FOLDER]
+                    file
+
+* You can either upload as an Anonymous user or use your credentials to login.
+  If no username & password combination is given, the file will be uploaded
+  anonymously.
+
+Dependencies
+============
+* python 2.x (http://python.org/)
+* requests (http://python-requests.org/)
+* lxml (http://lxml.de/)
+
+
+Todo
+====
+* Add options other than uploading:
+  * File getinfo, setinfo, delete, move
+  * Folder getinfo, setinfo, delete, move, create, content
+
+Notes
+=====
+It's not possible to have streaming uploads wuth the requests module and d-h.
+This means the whole file will be loaded to your memory before being uploaded.
+I might look into other modules if I have the time, but it's not a priority.
---
+++ devhost.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python2
+from requests import session
+from lxml import etree
+import os
+import binascii
+from StringIO import StringIO
+import argparse
+import time
+import json
+import threading
+import signal
+
+
+def arg_parser():
+    """Parses command line arguments, and returns a dict containing them."""
+    parser = argparse.ArgumentParser(description='d-h.st (Dev-Host) command line tool')
+    parser.add_argument('my_file', type=file, metavar='file', help='File to upload')
+    parser.add_argument('-u', '--username', help='Username')
+    parser.add_argument('-p', '--password', help='Password')
+    parser.add_argument('-d', '--file-desc', help='Description of file')
+    parser.add_argument('-c', '--file-code', help='File code of an existing file to update/replace')
+    parser.add_argument('-pb', '--public', choices=['0', '1'], default='0', help='File is public or private, 0 - private, 1 - public')
+    parser.add_argument('-f', '--upload-folder', default='0', help='Folder id to upload file too or specify 0 for root or leave blank implies root (0); if file_code is specified and valid then the location of the the file will change to the folder id specified')
+    args = parser.parse_args()
+    return vars(args)
+
+def upload_file(files_data, upload_data, xid):
+    """Uploads file and returns parsed response."""
+    # xid is optional, and can be used to track progress
+    if xid is None:
+        url = 'http://api.d-h.st/upload'
+    else:
+        url = 'http://api.d-h.st/upload?X-Progress-ID=%s' % xid
+    print "STARTING UPLOAD"
+    time.sleep(1)
+    r = s.post(url, data=upload_data, files=files_data)
+    # Parse the response, format then return it
+    resp = etree.parse(StringIO(r.content))
+    result = []
+    for field in resp.xpath("//file_info/*"):
+        result.append( "%s: %s" % (field.tag, field.text))
+    return result
+ 
+def gen_data(args, xid, token):
+    """Constructs data needed for the HTTP POST request."""
+    files_data = {'file': args['my_file']}
+    upload_data = {'action': "uploadapi", 'token': token,
+                   'public': args['public'],
+                   'upload_folder': args['upload_folder'],
+                   'file_description[]': args['file_desc'],
+                   'file_code[]': args['file_code']}
+    return files_data, upload_data
+
+def login(username, password):
+    """Login and return the token, which is used for identification."""
+    request = s.get('http://d-h.st/api/user/auth?user=%s&pass=%s' % (username, password))
+    content = request.content
+    resp = etree.parse(StringIO(content))
+    token = resp.xpath('//token/text()')[0]
+    return token
+
+def get_progress(xid):
+    """Gets the upload's progress, using the xid."""
+    while True:
+        time.sleep(5)
+        request = s.get('http://api.d-h.st/progress?X-Progress-ID=%s' % xid)
+        resp = request.content.strip()[1:-2]
+        progress = json.loads(resp)
+        if progress.get('state') == "uploading":
+            percentage = progress.get('received') / float(progress.get('size'))
+            print "Progress: %0.2f%%" % percentage
+        elif progress.get('state') == "starting":
+            pass
+        else:
+            print progress.get('state')
+
+def signal_handler(signal, frame):
+    """Handle SIGINT"""
+    print "\nAborted by user"
+    exit(0)
+
+
+signal.signal(signal.SIGINT, signal_handler)
+s = session()
+args = arg_parser()
+
+def upload(args):
+    """Handles file upload.
+    
+    Optionally handles authentication. Generates XID, builds request and calls
+    the upload_file method. Also runs get_progress as a thread to periodically
+    get the progress from the server.
+    """
+    token = None
+    xid = None
+    if args['username'] is not None and args['password'] is not None:
+        print "Logging in..."
+        token = login(args['username'], args['password'])
+    print "Uploading...\n"
+    xid = binascii.hexlify(os.urandom(8))
+    files_data, upload_data = gen_data(args, xid, token)
+    t = threading.Thread(target=get_progress, args=(xid,))
+    t.daemon = True
+    t.start()
+    result = upload_file(files_data, upload_data, xid)
+    for line in result:
+        print line
+
+# upload is the only function of this script right now, but this is likely to
+# change in the future.
+upload(args)
